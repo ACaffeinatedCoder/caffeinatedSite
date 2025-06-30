21d98487793a609a8328e6bfb0b7c851920f0919
Casey Francisco <caseyfrancisco13@gmail.com>
2025-06-30 12:40:52 +0800

    working on the projects page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,4 +1,11 @@
-import { faAt, faCircleXmark, faEnvelope, faLocation, faLocationDot, faMobile } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAt,
+  faCircleXmark,
+  faEnvelope,
+  faLocation,
+  faLocationDot,
+  faMobile,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Contact.css';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -6,11 +13,11 @@ import { useState } from 'react';
 
 export default function Contact({ closer }) {
   const copyToClipboard = (text, alertMessage) => {
-    setCopied(alertMessage)
-    navigator.clipboard.writeText(text)
-  }
+    setCopied(alertMessage);
+    navigator.clipboard.writeText(text);
+  };
 
-  const [copied, setCopied] = useState('')
+  const [copied, setCopied] = useState('');
 
   function showCopyToast() {
     const toast = document.getElementById('copy-toast');
@@ -25,9 +32,8 @@ export default function Contact({ closer }) {
     }, 2000);
   }
 
-
   return (
-    <div className='cont-container'>
+    <div className="cont-container">
       <div className="modal-header">
         <h1></h1>
         <FontAwesomeIcon
@@ -36,101 +42,105 @@ export default function Contact({ closer }) {
           onClick={() => closer()}
         />
       </div>
-      <div className='cont-subcontainer'>
-        <div className='cont-subheader'>
-            <div className='header-line'></div>
-            <h2>GET IN TOUCH</h2>
-            <div className='header-line'></div>
+      <div className="cont-subcontainer">
+        <div className="cont-subheader">
+          <div className="header-line"></div>
+          <h2>GET IN TOUCH</h2>
+          <div className="header-line"></div>
         </div>
-        <div className='contact-item-container'>
-          <div className='contact-item'
+        <div className="contact-item-container">
+          <div
+            className="contact-item"
             onClick={() => {
               copyToClipboard('caseyfrancisco13@gmail.com', 'email');
-              showCopyToast()
-            }}
-          >
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                className='contact-icon'
-              />
-              <p>caseyfrancisco13@gmail.com</p>
+              showCopyToast();
+            }}>
+            <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+            <p>caseyfrancisco13@gmail.com</p>
           </div>
-          <div className='contact-item'
+          <div
+            className="contact-item"
             onClick={() => {
               copyToClipboard('(+63) 999-8497301', 'mobile no');
-              showCopyToast()
-            }}
-          >
-              <FontAwesomeIcon
-                icon={faMobile}
-                className='contact-icon'
-              />
-              <p>(+63) 999-8497301</p>
+              showCopyToast();
+            }}>
+            <FontAwesomeIcon icon={faMobile} className="contact-icon" />
+            <p>(+63) 999-8497301</p>
           </div>
-          <div className='contact-item'
+          <div
+            className="contact-item"
             onClick={() => {
               copyToClipboard('Rizal, Philippines', 'address');
-              showCopyToast()
-            }}
-          >
-              <FontAwesomeIcon
-                icon={faLocationDot}
-                className='contact-icon'
-              />
-              <p>Rizal, Philippines</p>
-          </div> 
-          <div className='contact-item'
-            onClick={() => {
-              copyToClipboard('https://www.linkedin.com/in/casey-christian-francisco-92b9701bb/', 'LinkedIn url');
-              showCopyToast()
-            }}
-          >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className='contact-icon'
-              />
-              <p>LinkedIn</p>
+              showCopyToast();
+            }}>
+            <FontAwesomeIcon icon={faLocationDot} className="contact-icon" />
+            <p>Rizal, Philippines</p>
           </div>
-          <div className='contact-item'
+          <div
+            className="contact-item"
             onClick={() => {
-              copyToClipboard('https://github.com/ACaffeinatedCoder', 'Github profile url');
-              showCopyToast()
-            }}
-          >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className='contact-icon'
-              />
-              <p>Github</p>
+              copyToClipboard(
+                'https://www.linkedin.com/in/casey-christian-francisco-92b9701bb/',
+                'LinkedIn url'
+              );
+              showCopyToast();
+            }}>
+            <FontAwesomeIcon icon={faLinkedin} className="contact-icon" />
+            <p>LinkedIn</p>
+          </div>
+          <div
+            className="contact-item"
+            onClick={() => {
+              copyToClipboard(
+                'https://github.com/ACaffeinatedCoder',
+                'Github profile url'
+              );
+              showCopyToast();
+            }}>
+            <FontAwesomeIcon icon={faGithub} className="contact-icon" />
+            <p>Github</p>
           </div>
         </div>
-        
-        <div className='footer-line'></div>
+
+        <div className="footer-line"></div>
       </div>
       <div className="palette-container">
-          <h3 style={{marginRight: '10px', color:'black'}}>Website Palette:</h3>
-        {['#3e1e04', '#462003', '#6a3005', '#965015', '#c4923e', '#cbac85', '#ffd6a3', '#ffffff'].map(
-          (color, index) => {
-            const getTextColor = (bgColor) => {
-              const r = parseInt(bgColor.slice(1, 3), 16);
-              const g = parseInt(bgColor.slice(3, 5), 16);
-              const b = parseInt(bgColor.slice(5, 7), 16);
-              const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-              return brightness < 128 ? '#ffffff' : '#000000';
-            };
+        <h3 style={{ marginRight: '10px', color: 'black' }}>
+          Website Palette:
+        </h3>
+        {[
+          '#3e1e04',
+          '#462003',
+          '#6a3005',
+          '#965015',
+          '#c4923e',
+          '#cbac85',
+          '#ffd6a3',
+          '#ffffff',
+        ].map((color, index) => {
+          const getTextColor = (bgColor) => {
+            const r = parseInt(bgColor.slice(1, 3), 16);
+            const g = parseInt(bgColor.slice(3, 5), 16);
+            const b = parseInt(bgColor.slice(5, 7), 16);
+            const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+            return brightness < 128 ? '#ffffff' : '#000000';
+          };
 
-            const textColor = getTextColor(color);
+          const textColor = getTextColor(color);
 
-            return (
-              <div
-                key={index}
-                className="my-rectangle"
-                style={{ backgroundColor: color, color: textColor, fontWeight: 'bold' }}>
-                <p>{color}</p>
-              </div>
-            );
-          }
-        )}
+          return (
+            <div
+              key={index}
+              className="my-rectangle"
+              style={{
+                backgroundColor: color,
+                color: textColor,
+                fontWeight: 'bold',
+              }}>
+              <p>{color}</p>
+            </div>
+          );
+        })}
       </div>
       <div id="copy-toast" className="copy-toast hidden">
         Copied <b>{copied}</b> to clipboard!
--- src/Project.jsx
+++ src/Project.jsx
@@ -1,9 +1,112 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Experience.css';
-import './Project.css'
+import './Project.css';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export default function Project({ closer }) {
+  const [sites, setSites] = useState([
+    {
+      siteLink: 'https://punctuality-78586.web.app/',
+      title: 'PunctualITy',
+      roles: ['Fullstack'],
+      description: 'An RFID-enabled web app for logging and tracking student attendance in real time. Built with ReactJS and Firebase, it separates time-ins and time-outs, auto-generates timestamps, and stores records in the cloud for instant access and future analytics.',
+      awards: [],
+    },
+    {
+      siteLink: 'https://sctrack-f257d.web.app/',
+      title: 'SCTrack',
+      roles: ['Backend', 'Team Leader'],
+      description: 'A web-based ticketing system developed for Siena College of Taytay to streamline job order submissions. Built with ReactJS and Firebase, SCTrack significantly reduces manual effort by allowing users to log maintenance and service requests digitally, improving efficiency for both ITS and non-ITS personnel.',
+      awards: ['Best College Thesis', 'Albertus Magnus Research Award'],
+    },
+  ]);
+
+  const sitesMapped = sites.map((sites, index) => {
+    if (sites.awards.length !== 0) {
+      return (
+        <div
+          style={{
+            flexDirection: index % 2 === 0 ? 'row' : 'row-reverse',
+          }}
+          className="embed-container"
+          key={index}>
+          <div className="iframe-wrapper" style={{ flex: 2 }}>
+            <iframe
+              src={sites.siteLink}
+              style={{
+                transform: 'scale(1)',
+                transformOrigin: index % 2 === 0 ? 'top left' : 'top right',
+                width: '100%',
+                height: '54vh',
+                border: '5px solid #cbac85',
+                borderRadius: '15px',
+                margin: '10px',
+                pointerEvents: 'none',
+              }}
+            />
+          </div>
+          <div className="embed-details">
+            <h1>{sites.title}</h1>
+            <h3>Roles in the Development</h3>
+            <div className="proj-roles">
+              {sites.roles.map((role, index) => (
+                <p key={index}>{role}</p>
+              ))}
+            </div>
+            <div className='embed-visit'>
+                <button onClick={() => window.open(sites.siteLink, '_blank')}>Visit</button>
+            </div>            <p>{sites.description}</p>
+            <h2>Awards</h2>
+            <div className="proj-awards">
+              {sites.awards.map((award, index) => (
+                <p key={index}>{award}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          style={{
+            flexDirection: index % 2 === 0 ? 'row' : 'row-reverse',
+          }}
+          className="embed-container"
+          key={index}>
+          <div className="iframe-wrapper" style={{ flex: 2 }}>
+            <iframe
+              src={sites.siteLink}
+              style={{
+                transform: 'scale(1)',
+                transformOrigin: index % 2 === 0 ? 'top left' : 'top right',
+                width: '100%',
+                height: '54vh',
+                border: '5px solid #cbac85',
+                borderRadius: '15px',
+                margin: '10px',
+                pointerEvents: 'none',
+              }}
+            />
+          </div>
+          <div className="embed-details">
+            <h1>{sites.title}</h1>
+            <h3>Roles in the Development</h3>
+            <div className="proj-roles">
+              {sites.roles.map((role, index) => (
+                <p key={index}>{role}</p>
+              ))}
+            </div>
+            <div className='embed-visit'>
+                <button onClick={() => window.open(sites.siteLink, '_blank')}>Visit</button>
+            </div>
+            <p>{sites.description}</p>
+          </div>
+        </div>
+      );
+    }
+  });
+
   return (
     <div className="proj-container">
       <div className="modal-header">
@@ -16,6 +119,7 @@ export default function Project({ closer }) {
       </div>
       <div className="proj-subcontainer">
         <h1>Projects</h1>
+        <div>{sitesMapped}</div>
       </div>
     </div>
   );
