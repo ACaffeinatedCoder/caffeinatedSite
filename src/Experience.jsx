6c603ef6acf9fb0408f4787b52e670609bd2a650
Casey Francisco <caseyfrancisco13@gmail.com>
2025-07-03 12:42:06 +0800

    supabase has been integrated to Experience.jsx

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,235 +2,105 @@ import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 import './Experience.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
-import {
-  faGraduationCap,
-  faUserTie,
-} from '@fortawesome/free-solid-svg-icons';
+import { faGraduationCap, faUserTie } from '@fortawesome/free-solid-svg-icons';
 import { supabase } from './supabase-client';
 
 export default function Experience({ closer }) {
-  const [pages, setPages] = useState([
-    {
-      title: 'Experience',
-      description: 'Operations Support Analyst',
-      organization: 'Netzwelt, Inc.',
-      role: 'Operations Support Analyst',
-      subDetails: [
-        'Team & Customer Communication: Coordinate with colleagues daily via Teams or Zoom, and follow up with customers through email or phone regarding claims and inquiries.',
-        'Claim Validation & Support: Validate customer claims based on promotion mechanics and handle inquiries received through the support mailbox.',
-        'Process Improvement: Join process analysis meetings, identify areas for improvement, and maintain a strong understanding of internal workflows.',
-      ],
-      dateStart: 'March 2025',
-      dateEnd: 'May 2025',
-    },
-    {
-      title: 'Experience',
-      description: 'Part-time Course Instructor',
-      organization: 'Siena College of Taytay',
-      role: 'Part-time Course Instructor',
-      subDetails: [
-        'Teaching BSCS and BSIT students in Data Analysis with Python Programming',
-        'Teaching BSIT students in Systems Administration',
-        'Teaching BSCS students in Basic Web Development',
-        'Teaching BSIT students in Information Assurance and Security',
-      ],
-      dateStart: 'August 2024',
-      dateEnd: 'Now',
-    },
-    {
-      title: 'Education',
-      description: 'College',
-      organization: 'Siena College of Taytay',
-      role: 'Computer Science',
-      dateStart: 'August 2020',
-      dateEnd: 'May 2024',
-      subDetails: [
-        'Receiver of the Albertus Magnus Award',
-        'Receiver Best College Thesis',
-        "Dean's Lister",
-        'Founder of the Peer Tutoring Program',
-      ],
-    },
-    {
-      title: 'Education',
-      description: 'Senior High School',
-      organization: 'Siena College of Taytay',
-      role: 'STEM Strand',
-      dateStart: 'June 2018',
-      dateEnd: 'May 2020',
-      subDetails: [],
-    },
-  ]);
-
   const [experiences, setExperiences] = useState([]);
 
-  const getExp = async() => {
-    const {error, data} = await supabase.from("experience").select("*");
-    
+  const getExp = async () => {
+    const { error, data } = await supabase
+      .from('experience')
+      .select('*')
+      .order('date_start', { ascending: false });
+
     if (error) {
       console.error(error.message);
-      return
+      return;
     } else {
-      setExperiences(data)
+      setExperiences(data);
     }
-  }
+  };
 
   useEffect(() => {
-    getExp()
-  }, [])
+    getExp();
+  }, []);
 
   useEffect(() => {
-    console.log(experiences)
-  }, [experiences])
+    console.log(experiences);
+  }, [experiences]);
 
-  const experiencesMapped = experiences.map((exp) => {
-    return(
+  const expMapped = experiences.map((exp) => {
+    const isEducation = exp.exp_type === 'EDUCATION';
+    const hasDetails = exp.role_details.length !== 0;
+    const isCurrent = exp.exp_type === 'WORK' && exp.date_end === null;
+    const icon = isEducation ? faGraduationCap : faUserTie;
+    console.log(isCurrent);
+    const flagDate = new Date(exp.date_start).toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'long',
+    });
+
+    const fullStart = new Date(exp.date_start).toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    const fullEnd = exp.date_end
+      ? new Date(exp.date_end).toLocaleString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })
+      : 'Current';
+
+    return (
       <div className="exp-content-item" key={exp.id}>
-          <div className="exp-left">
-            <div className="time-tag">{exp.date_start}</div>
-            <div>FROM POSTGRE</div>
-          </div>
-          <div className="exp-middle">
-            <FontAwesomeIcon icon={faGraduationCap} className="modal-icon" />
-            <div className="timeline"></div>
-          </div>
-          <div className="exp-right">
-            <div className="exp-subHeader">
-              <h2>
-                {exp.organization} - {exp.role_description.toUpperCase()}
-              </h2>
-            </div>
-            <p>
-              From <i>{exp.date_start}</i> to <i>{exp.date_end}</i>
-            </p>
-            <h3>Highlights</h3>
-            <div className="exp-subcontent-item">
-              <ul>
-                {exp.role_details.map((ach, index) => (
-                  <li key={index}>{ach}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
+        <div className="exp-left">
+          <div className="time-tag">{flagDate}</div>
         </div>
-    )
-        
-  })
 
-  const pagesMapped = pages.map((content) => {
-    if (content.subDetails.length !== 0 && content.title === 'Education') {
-      return (
-        <div className="exp-content-item" key={content.role}>
-          <div className="exp-left">
-            <div className="time-tag">{content.dateStart}</div>
-          </div>
-          <div className="exp-middle">
-            <FontAwesomeIcon icon={faGraduationCap} className="modal-icon" />
-            <div className="timeline"></div>
-          </div>
-          <div className="exp-right">
-            <div className="exp-subHeader">
-              <h2>
-                {content.organization} - {content.description.toUpperCase()}
-              </h2>
-            </div>
-            <p>
-              From <i>{content.dateStart}</i> to <i>{content.dateEnd}</i>
-            </p>
-            <h3>Highlights</h3>
-            <div className="exp-subcontent-item">
-              <ul>
-                {content.subDetails.map((ach, index) => (
-                  <li key={index}>{ach}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
+        <div className="exp-middle">
+          <FontAwesomeIcon icon={icon} className="modal-icon" />
+          <div className="timeline"></div>
         </div>
-      );
-    } else if (content.title === 'Education') {
-      return (
-        <div className="exp-content-item" key={content.role}>
-          <div className="exp-left">
-            <div className="time-tag">{content.dateStart}</div>
+
+        <div className="exp-right">
+          <div className="exp-subHeader">
+            <h2>
+              {exp.organization} - {exp.role_description.toUpperCase()}
+            </h2>
           </div>
-          <div className="exp-middle">
-            <FontAwesomeIcon icon={faGraduationCap} className="modal-icon" />
-            <div className="timeline"></div>
-          </div>
-          <div className="exp-right">
-            <div className="exp-subHeader">
-              <h2>
-                {content.organization} - {content.description.toUpperCase()}
-              </h2>
-            </div>
-            <p>
-              From <i>{content.dateStart}</i> to <i>{content.dateEnd}</i>
-            </p>
-          </div>
+
+          <p>
+            From <i title={fullStart}>{flagDate}</i>{' '}
+            {isCurrent ? (
+              <>
+                and is <i>currently employed</i>
+              </>
+            ) : (
+              <>
+                to <i title={fullEnd}>{fullEnd}</i>
+              </>
+            )}
+          </p>
+
+          {hasDetails && (
+            <>
+              <h3>{isEducation ? 'Highlights' : 'Job Description'}</h3>
+              <div className="exp-subcontent-item">
+                <ul>
+                  {exp.role_details.map((ach, index) => (
+                    <li key={index}>{ach}</li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
         </div>
-      );
-    } else if (content.title === 'Experience' && content.dateEnd === 'Now') {
-      return (
-        <div className="exp-content-item" key={content.role}>
-          <div className="exp-left">
-            <div className="time-tag">{content.dateStart}</div>
-          </div>
-          <div className="exp-middle">
-            <FontAwesomeIcon icon={faUserTie} className="modal-icon" />
-            <div className="timeline"></div>
-          </div>
-          <div className="exp-right">
-            <div className="exp-subHeader">
-              <h2>
-                {content.organization} - {content.description.toUpperCase()}
-              </h2>
-            </div>
-            <p>
-              From <i>{content.dateStart}</i> and is <i>currently employed</i>            
-            </p>
-            <h3>Job Description</h3>
-            <div className="exp-subcontent-item">
-              <ul>
-                {content.subDetails.map((ach, index) => (
-                  <li key={index}>{ach}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      );
-    } else if (content.title === 'Experience') {
-      return (
-        <div className="exp-content-item" key={content.role}>
-          <div className="exp-left">
-            <div className="time-tag">{content.dateStart}</div>
-          </div>
-          <div className="exp-middle">
-            <FontAwesomeIcon icon={faUserTie} className="modal-icon" />
-            <div className="timeline"></div>
-          </div>
-          <div className="exp-right">
-            <div className="exp-subHeader">
-              <h2>
-                {content.organization} - {content.description.toUpperCase()}
-              </h2>
-            </div>
-            <p>
-              From <i>{content.dateStart}</i> to <i>{content.dateEnd}</i>
-            </p>
-            <h3>Job Description</h3>
-            <div className="exp-subcontent-item">
-              <ul>
-                {content.subDetails.map((ach, index) => (
-                  <li key={index}>{ach}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   });
 
   return (
@@ -244,7 +114,7 @@ export default function Experience({ closer }) {
           onClick={() => closer()}
         />
       </div>
-      <div className="exp-contents">{experiencesMapped}{pagesMapped}</div>
+      <div className="exp-contents">{expMapped}</div>
     </div>
   );
 }
