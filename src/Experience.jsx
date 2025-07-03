8d831ff10341fa3242fc8f13fce584d23099974d
Casey Francisco <caseyfrancisco13@gmail.com>
2025-07-03 11:24:03 +0800

    backing up before full integration

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,12 @@
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 import './Experience.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   faGraduationCap,
   faUserTie,
 } from '@fortawesome/free-solid-svg-icons';
+import { supabase } from './supabase-client';
 
 export default function Experience({ closer }) {
   const [pages, setPages] = useState([
@@ -60,6 +61,61 @@ export default function Experience({ closer }) {
       subDetails: [],
     },
   ]);
+
+  const [experiences, setExperiences] = useState([]);
+
+  const getExp = async() => {
+    const {error, data} = await supabase.from("experience").select("*");
+    
+    if (error) {
+      console.error(error.message);
+      return
+    } else {
+      setExperiences(data)
+    }
+  }
+
+  useEffect(() => {
+    getExp()
+  }, [])
+
+  useEffect(() => {
+    console.log(experiences)
+  }, [experiences])
+
+  const experiencesMapped = experiences.map((exp) => {
+    return(
+      <div className="exp-content-item" key={exp.id}>
+          <div className="exp-left">
+            <div className="time-tag">{exp.date_start}</div>
+            <div>FROM POSTGRE</div>
+          </div>
+          <div className="exp-middle">
+            <FontAwesomeIcon icon={faGraduationCap} className="modal-icon" />
+            <div className="timeline"></div>
+          </div>
+          <div className="exp-right">
+            <div className="exp-subHeader">
+              <h2>
+                {exp.organization} - {exp.role_description.toUpperCase()}
+              </h2>
+            </div>
+            <p>
+              From <i>{exp.date_start}</i> to <i>{exp.date_end}</i>
+            </p>
+            <h3>Highlights</h3>
+            <div className="exp-subcontent-item">
+              <ul>
+                {exp.role_details.map((ach, index) => (
+                  <li key={index}>{ach}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+    )
+        
+  })
 
   const pagesMapped = pages.map((content) => {
     if (content.subDetails.length !== 0 && content.title === 'Education') {
@@ -188,7 +244,7 @@ export default function Experience({ closer }) {
           onClick={() => closer()}
         />
       </div>
-      <div className="exp-contents">{pagesMapped}</div>
+      <div className="exp-contents">{experiencesMapped}{pagesMapped}</div>
     </div>
   );
 }
